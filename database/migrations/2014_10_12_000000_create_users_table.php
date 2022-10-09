7c8ac4b9df2faed988fb3da461a5479314193a72
Da Duon <da.d@pipay.com>
2022-10-09 15:11:20 +0700

    auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('fullname')->nullable();
+            $table->string('role')->default(1)->nullable();
+            $table->string('phone')->nullable();
+            $table->string('image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
