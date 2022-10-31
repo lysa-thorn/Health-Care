bb169d3bbbb6e2f6bd6f46bb68dcabf74a135152
lysa-thorn <lysa.thorn@gmail.com>
2022-10-31 22:59:49 +0700

    Add field user

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use App\Models\Material;
 
 class User extends Authenticatable implements JWTSubject
 {
@@ -68,5 +69,10 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims()
     {
         return [];
+    }
+
+    public function materials()
+    {
+        return $this->hasMany(Material::class);
     }
 }
